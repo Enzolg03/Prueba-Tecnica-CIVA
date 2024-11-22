888d39744c71b8ac2e6ddaef2ce3d23c7e00531c
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2024-11-22 12:54:18 -0500

    primer commit - agregando configuracion

---
+++ src/main/java/pe/com/civa/appbuses/AppbusesApplication.java
@@ -0,0 +1,13 @@
+package pe.com.civa.appbuses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbusesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppbusesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/civa/appbuses/AppbusesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.civa.appbuses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppbusesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
